0358a4bc29bc96bc294bb07633ff7029094710e2
markyv18 <markvanakkeren@gmail.com>
2016-12-08 08:43:12 -0700

    Update linked_list.rb

--- linked_list/lib/linked_list.rb
+++ linked_list/lib/linked_list.rb
@@ -3,6 +3,9 @@ class LinkedList
 attr_reader :head
 attr_reader :append
 attr_reader :data
+attr_reader :next_node
+attr_reader :to_string
+attr_reader :count
 
   def initialize
     @head = nil
@@ -17,6 +20,14 @@ attr_reader :data
   end
 
   def next_node
+  end
+
+  def count
+    v = 1  #??????
+  end
+
+  def to_string
+    "doop"
   end
 
 end
